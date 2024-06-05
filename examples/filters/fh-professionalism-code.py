3eb4a00cd4337afca876e7a81a754a02f50f2bc5
Robert McDermott <robert-mcdermott@users.noreply.github.com>
2024-06-05 10:41:45 -0700

    Update fh-professionalism-code.py

--- examples/filters/fh-professionalism-code.py
+++ examples/filters/fh-professionalism-code.py
@@ -77,8 +77,8 @@ class Pipeline:
         print(toxicity)
 
         if toxicity["toxicity"] > 0.5:
-            raise Exception("""Your prompt is a violation of the Fred Hutch standards of conduct: 'Treat others with respect, dignity and professionalism'
-                               Review the stardard here: ***https://centernet.fredhutch.org/u/compliance-office/standards.html***
+            raise Exception("""⚠ Your prompt is a violation of the Fred Hutch standards of conduct: 'Treat others with respect, dignity and professionalism'
+                               Review the standard here: https://centernet.fredhutch.org/u/compliance-office/standards.html
                                Repeated violations of these standards will result in loosing access to this system, or other actions.""")
 
         return body
