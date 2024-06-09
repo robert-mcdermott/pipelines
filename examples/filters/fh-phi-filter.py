de59139278467e54e6eef3ae247a8109e1b7f83e
Robert McDermott <robert-mcdermott@users.noreply.github.com>
2024-06-09 16:21:16 -0700

    Update fh-phi-filter.py

--- examples/filters/fh-phi-filter.py
+++ examples/filters/fh-phi-filter.py
@@ -8,42 +8,60 @@ requirements: requests
 """
 
 from typing import List, Optional
-from pydantic import BaseModel
 from schemas import OpenAIChatMessage
-
+from pydantic import BaseModel
+from transformers import pipeline
+import os
 
 class Pipeline:
     class Valves(BaseModel):
         pipelines: List[str] = []
         priority: int = 0
-        pass
 
     def __init__(self):
         self.type = "filter"
-        self.name = "Filter"
-        self.valves = self.Valves(**{"pipelines": ["llama3:latest"]})
+        self.name = "PHI Filter"
+
+        # Initialize
+        self.valves = self.Valves(
+            **{
+                "pipelines": ["*"],  # Connect to all pipelines
+            }
+        )
+        self.model = None
         pass
 
     async def on_startup(self):
+        # This function is called when the server is started.
         print(f"on_startup:{__name__}")
+
+        self.model = pipeline("token-classification", model="obi/deid_roberta_i2b2")
         pass
 
     async def on_shutdown(self):
+        # This function is called when the server is stopped.
         print(f"on_shutdown:{__name__}")
         pass
 
+    async def on_valves_updated(self):
+        # This function is called when the valves are updated.
+        pass
+
     async def inlet(self, body: dict, user: Optional[dict] = None) -> dict:
+        # This filter is applied to the form data before it is sent to the OpenAI API.
         print(f"inlet:{__name__}")
+
         print(body)
         user_message = body["messages"][-1]["content"]
 
-        # Filter out PHI
-        toxicity = self.model.predict(user_message)
-        print(toxicity)
+        # Filter out toxic messages
+        NER = []
+        results = self.model(user_message)
+        for res in results:
+            if res['score'] > 0.1:
+                NER.append(res)
 
-        if toxicity["toxicity"] > 0.5:
-            raise Exception("""⚠  Your prompt violates Fred Hutch's standards of conduct.
-                               💣 Repeated violations may result in loss of system access or other actions.
-                               👉 Review the standards here https://centernet.fredhutch.org/u/compliance-office/standards.html""")
+        if len(NER) > 0:
+            raise Exception("""💉PHI Detected🏥""")
 
         return body
